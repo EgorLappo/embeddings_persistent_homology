ca9234ae01f8420b1bd6c8fa36cf73896ca16186
Egor Alimpiev <alimpiev@stanford.edu>
2020-03-02 14:20:17 -0800

    proper pipeline

--- ripser_pipeline.py
+++ ripser_pipeline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import string
 import re
+from os import path
 
 import gensim
 from gensim.scripts.glove2word2vec import glove2word2vec
@@ -19,6 +20,7 @@ import argparse
 parser = argparse.ArgumentParser(description="embed the text as a point cloud in the embedding space")
 parser.add_argument('text', help='input text file')
 parser.add_argument('-s', '--save_numpy', action="store_true", help="save vectors in numpy format")
+parser.add_argument('-l', '--load_numpy', action="store_true", help="load vectors from numpy format")
 args = parser.parse_args()
 
 
@@ -69,73 +71,86 @@ def vectors_to_plex(vectors, filename):
             out.write(" ".join(map(str,vector)))
             out.write("\n")
 
+def ripser_compute(points):
+    return ripser(points, maxdim = 3)['dgms']
+
 if __name__ == "__main__":
     text_filename = args.text
+    text_name = args.text.split("/")[-1]
     text_words = tokenize_from_file(text_filename)
 
-    #word2vec
-    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', \
-                                                                     binary=True)  
-    word2vec_embeddings = get_vectors(word2vec_model, text_words)
-    if args.save_numpy:
-        np.save(text_filename+".word2vec", word2vec_embeddings)                                                           
+    print("read text of length "+len(text_words)+" words...")
 
-    #GLoVe
-    glove2word2vec(glove_input_file="glove.6B.300d.txt", word2vec_output_file="glove.6B.300d.gensim.txt")
-    glove2word2vec(glove_input_file="glove.840B.300d.txt", word2vec_output_file="glove.840B.300d.gensim.txt")
-    glove_cc_model = gensim.models.KeyedVectors.load_word2vec_format('glove.840B.300d.gensim.txt', binary=False)
-    glove_wiki_model = gensim.models.KeyedVectors.load_word2vec_format('glove.6B.300d.gensim.txt', binary=False)
+    if args.load_numpy:
+        word2vec_embeddings = np.load(text_name+".word2vec")
+        glove_wiki_embeddings = np.load(text_name".glove_wiki")
+        glove_cc_embeddings = np.load(text_name+".glove_cc")
+        elmo_embeddings = np.load(text_name+".elmo")
+        bert_embeddings = np.load(text_name+".bert")
+    else:
 
-    glove_cc_embeddings = get_vectors(glove_cc_model, text_words)
-    glove_wiki_embeddings = get_vectors(glove_wiki_model, text_words)
-    
-    if args.save_numpy:
-        np.save(text_filename+".glove_cc", glove_cc_embeddings)
-        np.save(text_filename+".glove_wiki", glove_wiki_embeddings)
+        #word2vec
+        print("embedding with word2vec...")
+        word2vec_model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', \
+                                                                         binary=True)  
+        word2vec_embeddings = get_vectors(word2vec_model, text_words)
+        if args.save_numpy:
+            np.save(text_name+".word2vec", word2vec_embeddings)                                                           
 
-    #ELMo
-    elmo_embedder = ElmoEmbedder(options_file = "elmo_2x2048_256_2048cnn_1xhighway_options.json", \
-                            weight_file = "elmo_2x2048_256_2048cnn_1xhighway_weights.hdf5")
+        #GLoVe
+        if not (path.exists("glove.6B.300d.gensim.txt") and path.exists("glove.840B.300d.gensim.txt")):
+            glove2word2vec(glove_input_file="glove.6B.300d.txt", word2vec_output_file="glove.6B.300d.gensim.txt")
+            glove2word2vec(glove_input_file="glove.840B.300d.txt", word2vec_output_file="glove.840B.300d.gensim.txt")
+        print("embedding with GloVe...")
+        glove_cc_model = gensim.models.KeyedVectors.load_word2vec_format('glove.840B.300d.gensim.txt', binary=False)
+        glove_wiki_model = gensim.models.KeyedVectors.load_word2vec_format('glove.6B.300d.gensim.txt', binary=False)
 
-    elmo_embeddings_raw = elmo_embedder.embed_sentence(text_words)
-
-    elmo_embeddings = elmo_embeddings_raw[2]
-
-    if args.save_numpy:
-        np.save(text_filename+".elmo",elmo_embeddings)
-
-    #BERT
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    with open(text_filename, "r") as f:
-        text = f.read()
-    tokenized_text = tokenizer.tokenize(text)
-
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-
-    tokens_tensor = torch.tensor([indexed_tokens])
-
-    model = BertModel.from_pretrained('bert-base-uncased')
-
-    model.eval()
-
-    with torch.no_grad():
-        outputs = model(tokens_tensor)
+        glove_cc_embeddings = get_vectors(glove_cc_model, text_words)
+        glove_wiki_embeddings = get_vectors(glove_wiki_model, text_words)
         
-        bert_embeddings = outputs[0][0]
-    
-    if args.save_numpy:    
-        np.save(text_filename+".bert", bert_embeddings.numpy())   
+        if args.save_numpy:
+            np.save(text_name+".glove_cc", glove_cc_embeddings)
+            np.save(text_name+".glove_wiki", glove_wiki_embeddings)
+
+        #ELMo
+        print("embedding with ELMo")
+        elmo_embedder = ElmoEmbedder(options_file = "elmo_2x2048_256_2048cnn_1xhighway_options.json", \
+                                weight_file = "elmo_2x2048_256_2048cnn_1xhighway_weights.hdf5")
+
+        elmo_embeddings_raw = elmo_embedder.embed_sentence(text_words)
+
+        elmo_embeddings = elmo_embeddings_raw[2]
+
+        if args.save_numpy:
+            np.save(text_name+".elmo",elmo_embeddings)
+
+        #BERT
+        print("embedding with GloVe...")
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        with open(text_name, "r") as f:
+            text = f.read()
+        tokenized_text = tokenizer.tokenize(text)
+        indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+        tokens_tensor = torch.tensor([indexed_tokens])
+        model = BertModel.from_pretrained('bert-base-uncased')
+        model.eval()
+
+        with torch.no_grad():
+            outputs = model(tokens_tensor)
+            bert_embeddings = outputs[0][0]
+        
+        if args.save_numpy:    
+            np.save(text_name+".bert", bert_embeddings.numpy())   
 
     # prepare for Plex
-    vectors_to_plex(word2vec_embeddings, filename="plex_input/"+text_filename+".word2vec_plex")
-    vectors_to_plex(glove_cc_embeddings, filename="plex_input/"+text_filename+".glove_cc_plex")
-    vectors_to_plex(glove_wiki_embeddings, filename="plex_input/"+text_filename+".glove_wiki_plex")
-    vectors_to_plex(elmo_embeddings, filename="plex_input/"+text_filename+".elmo_plex")
-    vectors_to_plex(bert_embeddings, filename="plex_input/"+text_filename+".bert_plex")
+    vectors_to_plex(word2vec_embeddings, filename="plex_input/"+text_name+".word2vec_plex")
+    vectors_to_plex(glove_cc_embeddings, filename="plex_input/"+text_name+".glove_cc_plex")
+    vectors_to_plex(glove_wiki_embeddings, filename="plex_input/"+text_name+".glove_wiki_plex")
+    vectors_to_plex(elmo_embeddings, filename="plex_input/"+text_name+".elmo_plex")
+    vectors_to_plex(bert_embeddings, filename="plex_input/"+text_name+".bert_plex")
 
     pool = multiprocessing.Pool(5)
-    diagrams =  pool.map(lambda x: ripser(x, maxdim = 3)['dgms'], [word2vec_embeddings, glove_wiki_embeddings, glove_cc_embeddings, elmo_embeddings, bert_embeddings])
+    diagrams =  pool.map(ripser_compute, [word2vec_embeddings, glove_wiki_embeddings, glove_cc_embeddings, elmo_embeddings, bert_embeddings])
 
     plt.figure(figsize=(25,5))
     plt.subplot(151)
